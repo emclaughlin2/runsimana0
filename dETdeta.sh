37b11a6d43a74dfd01fd375aabd12a6958b9bf78
emclaughlin2 <emlaugh1020@gmail.com>
2024-02-12 15:52:18 -0500

    Adding updates to run_dETdeta.C to allow for multiple generators and condor submit scripts for ampt and epos. Adding dst_files used for analysis so far to dst_files directory for documentation.

--- dETdeta.sh
+++ dETdeta.sh
@@ -2,7 +2,8 @@
 # file name: firstcondor.sh
 
 source /opt/sphenix/core/bin/sphenix_setup.sh -n
-source /opt/sphenix/core/bin/setup_local.sh "/sphenix/user/jocl/projects/testinstall"
-export TESTINSTALL="/sphenix/user/jocl/projects/testinstall"
-export HOME=/sphenix/u/jocl
-root -b -q 'run_dETdeta.C('${1},\"${2}\",${3},${4},${5},${6},${7}')'
+export HOME=/sphenix/u/egm2153
+export MYINSTALL=$HOME/install
+source $OPT_SPHENIX/bin/setup_local.sh $MYINSTALL
+export SPHENIX=$MYINSTALL
+root -b -q 'run_dETdeta.C('${1},\"${2}\",${3},${4},${5},${6},${7},${8}')'
--- run_dETdeta.C
+++ run_dETdeta.C
@@ -14,13 +14,13 @@
 #include <g4mbd/MbdDigitization.h>
 #include <mbd/MbdReco.h>
 #include <frog/FROG.h>
-#include <centrality/DansSpecialVertex.h>
 #include <ffamodules/CDBInterface.h>
 #include <fun4all/Fun4AllRunNodeInputManager.h>
+#include <centrality/CentralityReco.h>
+#include <calotrigger/MinimumBiasClassifier.h>
 using namespace std;
 
 R__LOAD_LIBRARY(libFROG.so)
-R__LOAD_LIBRARY(libdansspecialvertex.so)
 R__LOAD_LIBRARY(libg4jets.so)
 R__LOAD_LIBRARY(libg4vertex.so)
 R__LOAD_LIBRARY(libfun4all.so)
@@ -31,19 +31,31 @@ R__LOAD_LIBRARY(libg4mbd.so)
 R__LOAD_LIBRARY(libmbd_io.so)
 R__LOAD_LIBRARY(libmbd.so)
 R__LOAD_LIBRARY(libffamodules.so)
+R__LOAD_LIBRARY(libcentrality.so)
+R__LOAD_LIBRARY(libcalotrigger.so)
+
 bool file_exists(const char* filename)
 {
   std::ifstream infile(filename);
   return infile.good();
 }
-int run_dETdeta(int nproc = 0, string tag = "", int datormc = 0, int debug = 0, int nevt = 0, int correct = 1, int zs = 0)
+
+int run_dETdeta(int nproc = 0, string tag = "", int datormc = 0, int debug = 0, int nevt = 0, int correct = 1, int zs = 0, int runnumber = 23727)
 {
   int verbosity = 0;
   string filename = "output/evt/events_"+tag+(tag==""?"":"_");
-  string dattag = (datormc?"mc":"data");
+  string dattag;
+  switch(datormc) {
+    case 0: dattag = "data"; break;
+    case 1: dattag = "mc"; break;
+    case 2: dattag = "epos"; break;
+    case 3: dattag = "ampt"; break;
+    default: dattag = ""; std::cout << "data or mc generator value not valid" << std::endl; break;
+  }
   string cortag = (correct?"cor":"unc");
   filename += dattag + "_"+cortag+"_" + to_string(nproc);
   filename += ".root";
+  std::cout << filename << std::endl;
   FROG *fr = new FROG();
     
   gSystem->Load("libfun4all.so");
@@ -56,7 +68,10 @@ int run_dETdeta(int nproc = 0, string tag = "", int datormc = 0, int debug = 0, 
   se->Verbosity( verbosity );
   // just if we set some flags somewhere in this macro
   recoConsts *rc =  recoConsts::instance();
-  rc->set_uint64Flag("TIMESTAMP",21615);
+  rc->set_uint64Flag("TIMESTAMP",runnumber);
+  if (datormc == 0) { rc->set_StringFlag("CDB_GLOBALTAG","ProdA_2023"); }
+  else { rc->set_StringFlag("CDB_GLOBALTAG","MDC2"); } //"ProdA_2023");  "2023p007" 
+
   ifstream list1;
   string line1;
   ifstream list2;
@@ -65,22 +80,31 @@ int run_dETdeta(int nproc = 0, string tag = "", int datormc = 0, int debug = 0, 
   string line3;
   ifstream list4;
   string line4;
-  if(datormc)
-    {
-      if(!zs) list1.open("/sphenix/user/jocl/projects/sandbox/run/dst_calo_nozero.list");
-      else list1.open("/sphenix/user/jocl/projects/sandbox/run/dst_calo_cluster.list");
-      list2.open("/sphenix/user/jocl/projects/sandbox/run/dst_global.list");
-      list3.open("/sphenix/user/jocl/projects/sandbox/run/dst_truth.list");
-      list4.open("/sphenix/user/jocl/projects/sandbox/run/g4hits.list");
-    }
-  else
-    {
-      list1.open("/sphenix/user/jocl/projects/sandbox/run/dsts_production", ifstream::in);
-    }
-  for(int i=0; i<nproc+1; i++)
+  if (datormc == 0) {
+    string datafilelist = "/sphenix/user/egm2153/calib_study/detdeta/runsimana0/dst_files/dst_calo-000";
+    datafilelist += to_string(runnumber) + ".list";
+    list1.open(datafilelist);
+  } else if (datormc == 1) {
+    list1.open("/sphenix/user/egm2153/calib_study/detdeta/runsimana0/dst_files/dst_calo_nozero_hijing.list");
+    list2.open("/sphenix/user/egm2153/calib_study/detdeta/runsimana0/dst_files/dst_global_hijing.list");
+    list3.open("/sphenix/user/egm2153/calib_study/detdeta/runsimana0/dst_files/dst_truth_hijing.list");
+    list4.open("/sphenix/user/egm2153/calib_study/detdeta/runsimana0/dst_files/dst_mbd_epd_hijing.list");
+  } else if (datormc == 2) {
+    list1.open("/sphenix/user/egm2153/calib_study/detdeta/runsimana0/dst_files/dst_calo_nozero_epos.list");
+    list2.open("/sphenix/user/egm2153/calib_study/detdeta/runsimana0/dst_files/dst_global_epos.list");
+    list3.open("/sphenix/user/egm2153/calib_study/detdeta/runsimana0/dst_files/dst_truth_epos.list");
+    list4.open("/sphenix/user/egm2153/calib_study/detdeta/runsimana0/dst_files/dst_mbd_epd_epos.list");
+  } else if (datormc == 3) {
+    list1.open("/sphenix/user/egm2153/calib_study/detdeta/runsimana0/dst_files/dst_calo_nozero_ampt.list");
+    list2.open("/sphenix/user/egm2153/calib_study/detdeta/runsimana0/dst_files/dst_global_ampt.list");
+    list3.open("/sphenix/user/egm2153/calib_study/detdeta/runsimana0/dst_files/dst_truth_ampt.list");
+    list4.open("/sphenix/user/egm2153/calib_study/detdeta/runsimana0/dst_files/dst_mbd_epd_ampt.list");
+  }
+
+  for(int i=0; i<nproc+1; i++) 
     {
       getline(list1, line1);
-      if(datormc)
+      if(datormc > 0)
 	{
 	  getline(list4, line4);
 	  getline(list2, line2);
@@ -93,24 +117,22 @@ int run_dETdeta(int nproc = 0, string tag = "", int datormc = 0, int debug = 0, 
   Fun4AllInputManager *in_3 = new Fun4AllDstInputManager("DSTin3");
   Fun4AllInputManager *in_4 = new Fun4AllDstInputManager("DSTin4");
   in_1->AddFile(line1);
-  if(datormc)
+  if(datormc > 0)
     {
       in_2->AddFile(line2);
       in_3->AddFile(line3);
       in_4->AddFile(line4);
     }
   se->registerInputManager( in_1 );
-  if(datormc)
+  if(datormc > 0)
     {
       se->registerInputManager(in_2);
       se->registerInputManager(in_3);
       se->registerInputManager(in_4);
     }
-  // this points to the global tag in the CDB
-  rc->set_StringFlag("CDB_GLOBALTAG","2023p003");//"ProdA_2023");                                     
-// The calibrations have a validity range set by the beam clock which is not read out of the prdfs as of now
-  //rc->set_IntFlag("RANDOMSEED",158804);
+                                  
   int cont = 0;
+/*
   MbdDigitization* mbddigi;
   MbdReco* mbdreco;
   if(datormc)
@@ -120,7 +142,17 @@ int run_dETdeta(int nproc = 0, string tag = "", int datormc = 0, int debug = 0, 
       se->registerSubsystem(mbddigi);
       se->registerSubsystem(mbdreco);
     }
-  int runnumber = 21615;
+    */
+  if(!datormc) {
+    MinimumBiasClassifier *mb = new MinimumBiasClassifier();
+    mb->Verbosity(verbosity);
+    se->registerSubsystem(mb);
+  }
+  
+  CentralityReco* cent = new CentralityReco();
+  cent->Verbosity(verbosity);
+  se->registerSubsystem( cent ); 
+  
   /*
   DansSpecialVertex *dsv;
   if(!datormc)
@@ -137,6 +169,18 @@ int run_dETdeta(int nproc = 0, string tag = "", int datormc = 0, int debug = 0, 
   std::string geoLocation = cdb->getUrl("calo_geo");
   intrue2->AddFile(geoLocation);
   se->registerInputManager(intrue2);
+
+  // option for testing new emcal calibrations with direct URL instead of CDB
+  /*
+  if (!datormc) {
+    std::cout << "Calibrating EMCal" << std::endl;
+    CaloTowerCalib *calibEMC = new CaloTowerCalib("CEMCCALIB");
+    calibEMC->set_detector_type(CaloTowerDefs::CEMC);
+    calibEMC->set_directURL("/sphenix/user/egm2153/calib_study/detdeta/testing/new_emcal_calib.root");
+    se->registerSubsystem(calibEMC);
+  }
+  */
+  // option for testing new emcal calibrations with direct URL instead of CDB
 
   MDCTreeMaker *tt = new MDCTreeMaker( filename, datormc, debug, correct );
   se->registerSubsystem( tt );
